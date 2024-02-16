05f25b8d3a9afe7fde6f7a120e983472b9e3487b
hwangyunje <dnjy1945@naver.com>
2024-02-17 00:17:19 +0900

    read

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,13 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
 @Slf4j
 @Controller
 public class ArticleController {
@@ -26,4 +31,12 @@ public class ArticleController {
         log.info(saved.toString());
         return "";
     }
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id,Model model){
+        log.info("id= " + id);
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+        return "articles/show";
+    }
+
 }
