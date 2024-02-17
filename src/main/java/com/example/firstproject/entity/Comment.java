f8fe05fcc7fde409a174839f57de3bdd5aa8f86f
hwangyunje <dnjy1945@naver.com>
2024-02-18 02:05:49 +0900

    comment api

--- src/main/java/com/example/firstproject/entity/Comment.java
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.entity;
 
+import com.example.firstproject.dto.CommentDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,4 +23,20 @@ public class Comment {
     private String nickname;
     @Column
     private String body;
+
+    public static Comment createComment(CommentDto dto, Article article) {
+        if(dto.getId() != null)
+            throw new IllegalArgumentException("댓글생성실패 댓글의 id가 없어야합니다.");
+
+        if(dto.getArticleId()!=article.getId())
+            throw new IllegalArgumentException("댓글생성실패 게시글의 id가 잘못됐습니다.");
+
+        return new Comment(
+                dto.getId(),
+                article,
+                dto.getNickname(),
+                dto.getBody()
+        );
+
+    }
 }
