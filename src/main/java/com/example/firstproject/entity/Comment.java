e580ea8d81637946ef3ca4eee8bcb238be948262
hwangyunje <dnjy1945@naver.com>
2024-02-17 21:11:19 +0900

    test

---
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -0,0 +1,25 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name="article_id")
+    private Article article;
+    @Column
+    private String nickname;
+    @Column
+    private String body;
+}
