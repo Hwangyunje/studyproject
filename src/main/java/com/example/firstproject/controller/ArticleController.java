c577fc51189499479bbe561dedcf8f07f54bf5c0
hwangyunje <dnjy1945@naver.com>
2024-02-17 13:39:54 +0900

    update,delete

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,7 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -29,7 +32,7 @@ public class ArticleController {
         log.info(article.toString());
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId();
     }
     @GetMapping("/articles/{id}")
     public String show(@PathVariable Long id,Model model){
@@ -39,7 +42,38 @@ public class ArticleController {
         return "articles/show";
     }
 @GetMapping("/articles")
-    public String index(){
-        return "";
+    public String index(Model model){
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        model.addAttribute("articleList",articleEntityList);
+        return "articles/index";
+    }
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id,Model model){
+        Article articleEntiry = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article",articleEntiry);
+        return "articles/edit";
+    }
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if (target !=null){
+            articleRepository.save(articleEntity);
+        }
+
+        return "redirect:/articles/"+ articleEntity.getId();
+    }
+    @GetMapping("articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제요청");
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        if(target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제됐습니다");
+        }
+        return "redirect:/articles";
     }
 }
--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class ArticleForm {
+    private Long id;
     private String title;
     private String content;
 
@@ -14,6 +15,6 @@ public class ArticleForm {
 
 
     public Article toEntity() {
-        return new Article(null,title,content);
+        return new Article(id,title,content);
     }
 }
